148977dfa02e6151bb39892b96bfac83586d945a
thomas <thomasjefer404@gmail.com>
2025-08-02 20:32:45 -0300

    adicionado a header basico

---
+++ basic.c
@@ -0,0 +1,5 @@
+//
+// Created by Thomas on 02/08/2025.
+//
+
+#include "basic.h"
---
+++ basic.h
@@ -0,0 +1,8 @@
+//
+// Created by Thomas on 02/08/2025.
+//
+
+#ifndef BASIC_H
+#define BASIC_H
+
+#endif //BASIC_H
